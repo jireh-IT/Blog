680899443c4b468981b97d841ef8a04ddece5abc
Jireh-IT <kapangajireh@gmail.com>
2023-08-25 21:29:34 +0100

    showing post on home page

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
-import { Subject } from 'rxjs';
+import {ActivatedRoute, Data, ParamMap, Router, RouterModule} from '@angular/router';
+import {combineLatest, Observable, Subject, switchMap, tap} from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 import SharedModule from 'app/shared/shared.module';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
+import {EntityArrayResponseType, PostService} from "../entities/post/service/post.service";
+import {IPost} from "../entities/post/post.model";
+import {SortService} from "../shared/sort/sort.service";
+import {ASC, DEFAULT_SORT_DATA, DESC, SORT} from "../config/navigation.constants";
 
 @Component({
   standalone: true,
@@ -14,26 +18,71 @@ import { Account } from 'app/core/auth/account.model';
   styleUrls: ['./home.component.scss'],
   imports: [SharedModule, RouterModule],
 })
-export default class HomeComponent implements OnInit, OnDestroy {
-  account: Account | null = null;
+export default class HomeComponent implements OnInit {
 
-  private readonly destroy$ = new Subject<void>();
+  posts : IPost[] = [];
 
-  constructor(private accountService: AccountService, private router: Router) {}
+  isLoading = false;
+
+  predicate = 'id';
+  ascending = true;
+
+
+  constructor(private postService: PostService,
+              private router: Router,
+              protected sortService: SortService,
+              protected activatedRoute: ActivatedRoute
+              ) {}
 
   ngOnInit(): void {
-    this.accountService
-      .getAuthenticationState()
-      .pipe(takeUntil(this.destroy$))
-      .subscribe(account => (this.account = account));
+     this.loadAll();
+  }
+
+  loadAll(): void {
+    const queryObject: any = {
+      sort: this.getSortQueryParam(this.predicate, this.ascending),
+    };
+    this.postService.query(queryObject).subscribe(
+      res => {
+
+        this.onResponseSuccess(res);
+      }
+    );
+
+  }
+
+
+
+
+
+
+
+  protected onResponseSuccess(response: EntityArrayResponseType): void {
+    const dataFromBody = this.fillComponentAttributesFromResponseBody(response.body);
+    this.posts = this.refineData(dataFromBody);
+
+  }
+
+  protected fillComponentAttributesFromResponseBody(data: IPost[] | null): IPost[] {
+    return data ?? [];
+  }
+
+  protected refineData(data: IPost[]): IPost[] {
+    return data.sort(this.sortService.startSort(this.predicate, this.ascending ? 1 : -1));
+  }
+
+  protected getSortQueryParam(predicate = this.predicate, ascending = this.ascending): string[] {
+    const ascendingQueryParam = ascending ? ASC : DESC;
+    if (predicate === '') {
+      return [];
+    } else {
+      return [predicate + ',' + ascendingQueryParam];
+    }
   }
 
   login(): void {
     this.router.navigate(['/login']);
   }
 
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
+
 }
